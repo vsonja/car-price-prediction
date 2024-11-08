f1339971d9be0504405e303a282094102c0c13c7
vsonja <153772075+vsonja@users.noreply.github.com>
2024-11-08 14:12:28 +0200

    Parse data from CSV file into sequence of vectors

--- src/car_price_prediction/core.clj
+++ src/car_price_prediction/core.clj
@@ -1,7 +1,9 @@
 (ns car-price-prediction.core
-  (:gen-class))
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn read-csv-file [file-path]
+  (with-open [reader (io/reader file-path)]
+    (doall (csv/read-csv reader))))
+
+(def data (read-csv-file "resources/cars.csv"))
