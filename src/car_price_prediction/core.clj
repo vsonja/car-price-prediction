075893d302a90e00f479521d2decfa4fd4a1d9b0
vsonja <153772075+vsonja@users.noreply.github.com>
2024-11-14 10:15:54 +0200

    Add Command-Line Menu and implement Search

--- src/car_price_prediction/core.clj
+++ src/car_price_prediction/core.clj
@@ -4,6 +4,60 @@
 
 (defn read-csv-file [file-path]
   (with-open [reader (io/reader file-path)]
-    (doall (csv/read-csv reader))))
+    (let [rows (csv/read-csv reader)
+          headers (map keyword (first rows))]
+      (doall (map #(zipmap headers %) (rest rows))))))
 
 (def data (read-csv-file "resources/cars.csv"))
+
+(defn main-menu []
+  (println "\nMenu Options:")
+  (println "1. View all cars")
+  (println "2. Search for specific car")
+  (println "3. Exit")
+  (println "Select an option: ")
+  (flush))
+
+(defn view-all []
+  (println "\nDisplaying all cars...")
+  (doseq [row data]
+    (println row)))
+
+(defn search []
+  (println "")
+  (let [columns (remove #{:price} (keys (first data)))
+        criteria (reduce
+                  (fn [acc column]
+                    (println (str "Enter value for '" (name column) "' (or press Enter to skip): "))
+                    (flush)
+                    (let [value (read-line)]
+                      (if (empty? value)
+                        acc
+                        (assoc acc column value))))
+                  {}
+                  columns)
+
+        results (filter
+                 (fn [row]
+                   (every? (fn [[column value]]
+                             (= (get row column) value))
+                           criteria))
+                 data)]
+
+    (if (seq results)
+      (do
+        (println "\nResults matching search criteria:")
+        (doseq [row results]
+          (println row)))
+      (println "\nNo matching results."))))
+
+(defn -main [& args]
+  (println "Welcome to the Car Price Prediction App!")
+  (loop []
+    (main-menu)
+    (let [option (read-line)]
+      (cond
+        (= option "1") (do (view-all) (recur))
+        (= option "2") (do (search) (recur))
+        (= option "3") (println "Goodbye!")
+        :else (do (println "Invalid option! Please try again.") (recur))))))
