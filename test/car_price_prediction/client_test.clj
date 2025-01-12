10bfd703a72ef7c2b365201969f85fdeced06f54
vsonja <153772075+vsonja@users.noreply.github.com>
2025-01-12 11:49:33 +0100

    Add tests for fetch-car-data, fetch-price-statistics and vin-decoder

---
+++ test/car_price_prediction/client_test.clj
@@ -0,0 +1,24 @@
+(ns car-price-prediction.client-test
+  (:require [midje.sweet :refer :all]
+            [clojure.test :refer :all]
+            [car-price-prediction.client :refer :all]))
+
+(facts "Search active dealer inventory using Search API."
+       (fetch-car-data "Volkswagen" "Golf" "2020") =not=> nil
+
+       (fact "Invalid value."
+             (fetch-car-data "Volkswagen" "Golf" "/") => nil
+             (with-out-str (fetch-car-data "Volkswagen" "Golf" "/")) => (contains "Failed to fetch data:")))
+
+(facts "Fetch price statistics using Search API."
+       (:price (fetch-price-statistics "Volkswagen" "Golf" "2020" "20000-40000" "Unleaded" "1.4L I4" "Automatic")) =not=> (contains {:count 0})
+
+       (fact "Invalid value."
+             (:price (fetch-price-statistics "Volkswagen" "Golf" "2020" "20000-40000" "Unleaded" "1.4L I4" "/")) => (contains {:count 0})))
+
+(facts "Decode VIN and get the specifications for it using VIN Decoder API."
+       (vin-decoder "1FAHP3F28CL148530") =not=> nil
+
+       (fact "Invalid value."
+             (vin-decoder "") => nil
+             (with-out-str (vin-decoder "")) => (contains "Failed to decode VIN:")))
